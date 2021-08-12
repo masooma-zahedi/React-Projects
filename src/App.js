dcce0c0e995d2408ee95e0625c84ad0f8c4aeae5
masooma <masooma.zahedi1@gmil.com>
2021-08-12 07:19:45 -0500

    start project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
-import Alert from './Alert'
+import Alert from './Alert';
+import Bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import { nanoid } from 'nanoid';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [editable, setEditable] = useState(false)
+  const [list, setList] = useState([]);
+  const [edit, setEdit] = useState(null)
+  const [alert, setAlert] = useState({show:false, msg: "", type:""})
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(!name){
+      // display alert
+      setAlert({show:true, msg:"hahaha",type:"no "})
+    }
+    else if(name && editable){
+      // deal with edit
+    } 
+    else{
+      const newItem = {id:nanoid(4), title:name}
+      setList([...list, newItem]);
+      setName("")
+
+    }
+  }
+  return (
+    <>
+      <section className="container mt-5">
+        <div className="main border shadow-lg">
+          <h3 className="text-center my-3">Grocery Bud</h3>
+          <form onSubmit={handleSubmit} className="form-group align-middle text-center">
+            <div className="input-group justify-content-center mb-2 w-75 mx-auto">
+              <input type="text" className="form-control px-1 border " placeholder="e.g eggs" value={name} onChange={(e)=>{setName(e.target.value)}} />
+              <button type='submit' value="" className="btn btn-primary btn-sm ">
+                {editable ? "Edit" : "Submit"}
+              </button>
+            </div>
+          </form>
+          {list.length > 0 && 
+          <div className="text-center">
+            <List items={list} />
+            <button
+              onClick={()=>{setList([])}}
+             className=" text-danger border-0"  style={{backgroundColor:"transparent"}}>Clear Items</button>
+          </div>
+          }
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App
--- src/List.js
+++ src/List.js
@@ -1,7 +1,37 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items }) => {
 
-export default List
+  const handleEdit=()=>{
+
+  }
+  return (
+    <div>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <div
+            key={id}
+            className="border text-start px-4 w-75 mx-auto d-flex justify-content-between
+            "
+          >
+            <p className="mb-0">{title}</p>
+            <p>{id}</p>
+            <div className=" ">
+              <button className="mx-2 border-0 outline-none" style={{backgroundColor:"transparent"}}>
+                <FaEdit
+                  onClick={handleEdit}
+                className="text-success border-0" />
+              </button>
+              <button className=" border-0 outline-none" style={{backgroundColor:"transparent"}}>
+                <FaTrash className="text-danger" />
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
